ee42261ce2689facac5effb569fc1e9d98e4debe
가린 <69245660+ryoo0930@users.noreply.github.com>
2024-11-19 11:17:25 +0900

    2×n 타일링
    SILVER 3

---
+++ 11726.c
@@ -0,0 +1,21 @@
+//11726
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int widthSize = 0;
+	scanf("%d", &widthSize);
+	int* dp = (int*)calloc(widthSize + 1, sizeof(int));
+	dp[1] = 1;
+	if (widthSize >= 2) {
+		dp[2] = 2;
+	}
+	for (int i = 3; i <= widthSize; i++) {
+		dp[i] = (dp[i - 2] + dp[i - 1])%10007;
+	}
+
+	printf("%d", dp[widthSize]);
+
+	return 0;
+}
