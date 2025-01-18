3aa43e8d69315c1e2bfe04437a141bd119a6e019
가린 <69245660+ryoo0930@users.noreply.github.com>
2025-01-18 19:52:00 +0900

    [한글화] 헤더파일
    16495.cpp Header

---
+++ 16495.한글화/내가만든헤더~.h
@@ -0,0 +1,14 @@
+#ifndef KKOR_H
+#define KKOR_H
+
+#pragma once
+
+#define 기본설정 using namespace std;
+#define 입력 cin
+#define 출력 cout
+#define 반복 for
+#define 만약 if
+#define 길이 length
+#define 제곱 pow
+
+#endif
