54241c212ea7677147eb7fd41cba12a596b5540f
Assva <69245660+ryoo0930@users.noreply.github.com>
2025-05-16 16:47:55 +0900

    Create 5043.c
    SILVER 5

---
+++ 5043.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+	unsigned long long fileSize = 0;
+	int maxBit = 0;
+	scanf("%lld %d", &fileSize, &maxBit);
+
+	int needBitSize = 0;
+	while (fileSize != 0) {
+		fileSize = fileSize / 2;
+		needBitSize++;
+	}
+
+	if (needBitSize <= maxBit + 1) printf("yes\n");
+	else printf("no\n");
+
+	return 0;
+}
